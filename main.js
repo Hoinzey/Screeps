8f930bf2b65fe0a419e8cb86b914079a3c4a99aa
Darren Hoyne <hoinzey2007@yahoo.ie>
2018-02-21 21:58:08 +0000

    w

--- main.js
+++ main.js
@@ -9,58 +9,49 @@ var roomSetup = require('roomSetup');
 
 module.exports.loop = function () {
     // profiler.wrap(function() {
-    
-    if(Game.flags.Siege!=undefined){
-        if(Memory.siegeRoom==undefined){
-            let siegeObject = {name: Game.flags.Siege.pos.roomName,scouted:false,enemiesPresent:false,claim: false,claimed: false,hostileCount: 0};
-            Memory.siegeRoom = siegeObject;
-        }
-    }else{
-        Memory.siegeRoom = undefined;
-    }
     for(var currentRoomName in Game.rooms){
         var currentRoom = Game.rooms[currentRoomName];
-        if(currentRoom.memory.setupComplete==undefined){
+        // if(currentRoom.memory.setupComplete==undefined){
             roomSetup.run(currentRoom);
-            currentRoom.memory.setupComplete = true;
-        }
+            // currentRoom.memory.setupComplete = true;
+        // }
         // Testwwww
-        currentRoom.memory.structures = currentRoom.find(FIND_STRUCTURES);
-        roomFoundations.buildRoom(currentRoom);
         // currentRoom.memory.structures = currentRoom.find(FIND_STRUCTURES);
-        creepFactory.run(currentRoom);
-        creepHandler.run(currentRoom);
+        // roomFoundations.buildRoom(currentRoom);
+        // currentRoom.memory.structures = currentRoom.find(FIND_STRUCTURES);
+        // creepFactory.run(currentRoom);
+        // creepHandler.run(currentRoom);
     }
     
-    var tower = Game.getObjectById('58a8a84fca7c6f4d60332527');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.structureType!=STRUCTURE_WALL && structure.hits < structure.hitsMax
-        });
-        // console.log("Tower targeting "+closestDamagedStructure.structureType)
-        if(closestDamagedStructure && tower.energy > (tower.energyCapacity/100)*30) {
-            tower.repair(closestDamagedStructure);
-        }
+    // var tower = Game.getObjectById('58a8a84fca7c6f4d60332527');
+    // if(tower) {
+    //     var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+    //         filter: (structure) => structure.structureType!=STRUCTURE_WALL && structure.hits < structure.hitsMax
+    //     });
+    //     // console.log("Tower targeting "+closestDamagedStructure.structureType)
+    //     if(closestDamagedStructure && tower.energy > (tower.energyCapacity/100)*30) {
+    //         tower.repair(closestDamagedStructure);
+    //     }
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
-    var tower2 = Game.getObjectById('58ac1c5623b1355f5f528265');
-    if(tower2) {
-        var closestDamagedStructure = tower2.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax //structure.structureType!=STRUCTURE_WALL &&
-        });
-        // console.log("Tower targeting "+closestDamagedStructure.structureType)
-        if(closestDamagedStructure && tower2.energy > (tower2.energyCapacity/100)*50) {
-            tower2.repair(closestDamagedStructure);
-        }
+    //     var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    //     if(closestHostile) {
+    //         tower.attack(closestHostile);
+    //     }
+    // }
+    // var tower2 = Game.getObjectById('58ac1c5623b1355f5f528265');
+    // if(tower2) {
+    //     var closestDamagedStructure = tower2.pos.findClosestByRange(FIND_STRUCTURES, {
+    //         filter: (structure) => structure.hits < structure.hitsMax //structure.structureType!=STRUCTURE_WALL &&
+    //     });
+    //     // console.log("Tower targeting "+closestDamagedStructure.structureType)
+    //     if(closestDamagedStructure && tower2.energy > (tower2.energyCapacity/100)*50) {
+    //         tower2.repair(closestDamagedStructure);
+    //     }
 
-        var closestHostile = tower2.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower2.attack(closestHostile);
-        }
-    }
+    //     var closestHostile = tower2.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    //     if(closestHostile) {
+    //         tower2.attack(closestHostile);
+    //     }
+    // }
     // });
 }
