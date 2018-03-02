d5fcf96c363e3e1375df756d48936fea0f0c3c12
Darren Hoyne <hoinzey2007@yahoo.ie>
2018-03-02 19:35:12 +0000

    b

--- roomFoundations.js
+++ roomFoundations.js
@@ -22,24 +22,16 @@ var roomFoundations = {
         // conSites.forEach(function(site){
         //     site.remove();
         // })
-        // this.buildPaths(currentRoom);
-        // this.buildContainers(currentRoom);
-        // this.buildExtensions(currentRoom);
-        // if(currentRoom.memory.spawnInfo==undefined){
-            
-        // }
-        // this.buildExtensions(currentRoom);
-        // this.buildContainers(currentRoom);
         if(currentRoom.memory.lastCheckControllerLevel!=undefined){
             if(currentRoom.memory.lastCheckControllerLevel < currentRoom.controller.level){
-                this.buildPaths(currentRoom);
+                // this.buildPaths(currentRoom);
                 this.buildExtensions(currentRoom);
                 this.buildContainers(currentRoom);
                 currentRoom.memory.lastCheckControllerLevel = currentRoom.controller.level;
             }
         }else{
             currentRoom.memory.lastCheckControllerLevel=0;
-            this.buildPaths(currentRoom);
+            // this.buildPaths(currentRoom);
             try{
               this.buildContainers(currentRoom);
               this.buildExtensions(currentRoom);
@@ -48,33 +40,33 @@ var roomFoundations = {
             }
         }
     },
-    buildPaths : function(currentRoom){
-        _.each(currentRoom.find(FIND_MY_SPAWNS),function(spawn){
-            let spawnInformation = currentRoom.memory.spawnInfo;
-            // console.log(spawnInformation);
-            if(spawnInformation==undefined){
-                let spawnObject = {x:spawn.pos.x,y:spawn.pos.y};
-                let  spawnInfo={};
-                spawnInfo[spawn.id] = spawnObject;
-                currentRoom.memory.spawnInfo=spawnInfo;
-            }else if(!_.contains(spawnInformation,spawnInformation[spawn.id])){
-                let spawnObject = {x:spawn.pos.x,y:spawn.pos.y};
-                currentRoom.memory.spawnInfo[spawn.id] = spawnObject;
-            }
-            let targets = pathTargets(currentRoom);
-            // console.log(targets.length);
-            _.each(targets,function(target){
-                let tarPos = new RoomPosition(target.x,target.y,currentRoom.name);
-                let pathTiles = currentRoom.findPath(spawn.pos,tarPos,{ignoreCreeps:true});
-                pathTiles.forEach(function(dest){
-                    // if(currentRoom.createConstructionSite(dest.x,dest.y,STRUCTURE_ROAD) != 0){
-                    // }
-                    currentRoom.createFlag(dest.x,dest.y,null,COLOR_RED);
-                });
-            })
+    // buildPaths : function(currentRoom){
+    //     _.each(currentRoom.find(FIND_MY_SPAWNS),function(spawn){
+    //         let spawnInformation = currentRoom.memory.spawnInfo;
+    //         // console.log(spawnInformation);
+    //         if(spawnInformation==undefined){
+    //             let spawnObject = {x:spawn.pos.x,y:spawn.pos.y};
+    //             let  spawnInfo={};
+    //             spawnInfo[spawn.id] = spawnObject;
+    //             currentRoom.memory.spawnInfo=spawnInfo;
+    //         }else if(!_.contains(spawnInformation,spawnInformation[spawn.id])){
+    //             let spawnObject = {x:spawn.pos.x,y:spawn.pos.y};
+    //             currentRoom.memory.spawnInfo[spawn.id] = spawnObject;
+    //         }
+    //         let targets = pathTargets(currentRoom);
+    //         // console.log(targets.length);
+    //         _.each(targets,function(target){
+    //             let tarPos = new RoomPosition(target.x,target.y,currentRoom.name);
+    //             let pathTiles = currentRoom.findPath(spawn.pos,tarPos,{ignoreCreeps:true});
+    //             pathTiles.forEach(function(dest){
+    //                 // if(currentRoom.createConstructionSite(dest.x,dest.y,STRUCTURE_ROAD) != 0){
+    //                 // }
+    //                 currentRoom.createFlag(dest.x,dest.y,null,COLOR_RED);
+    //             });
+    //         })
             
-        })
-    },
+    //     })
+    // },
     buildContainers : function(currentRoom){
         let containerAllowance = 0;
         for(var count=0; count <= currentRoom.controller.level; count++ ){
@@ -181,31 +173,6 @@ function checkAdjacentSquaresForExtensionOrWall(x,y,currentRoom){
      return validTile && hasSomethingBesideIt;
 }
 
-function pathTargets(currentRoom){
-        var targets = [];
-        currentRoom.find(FIND_SOURCES).forEach(function(source){
-            targets.push({x:source.pos.x,y:source.pos.y});
-        });
-        currentRoom.find(FIND_MY_SPAWNS).forEach(function(spawn){
-            let offset=1;
-            let xPos = spawn.pos.x - offset;
-            let yPos = spawn.pos.y - offset;
-            for(var x =xPos; x<=spawn.pos.x+offset ;x++){
-                for(var y=yPos;y<=spawn.pos.y+offset;y++){
-                    //  if(x==spawn.pos.x+1 && y==spawn.pos.y+1){
-                    //     currentRoom.createConstructionSite(x,y,STRUCTURE_CONTAINER);
-                    //     continue;
-                    // }
-                    // currentRoom.createFlag(x,y);
-                    let obj = {x:x,y:y};
-                    // console.log(obj.x + " "+ obj.y);
-                    targets.push(obj);
-                }
-            }
-            targets.push({x:spawn.pos.x,y:spawn.pos.y});
-        });
-        targets.push({x:currentRoom.controller.pos.x,y:currentRoom.controller.pos.y});
-        return targets;
-    }
+
 
 module.exports = roomFoundations;
--- roomSetup.js
+++ roomSetup.js
@@ -2,6 +2,7 @@ var roomSetup={
     run : function(currentRoom){
         //Set up the rooms memory objects such as locations of sources
         commitRoomSourcesAndSpawnsToMemory(currentRoom);
+        buildPaths(currentRoom);
     }
 };
 
@@ -40,5 +41,60 @@ function commitRoomSourcesAndSpawnsToMemory(currentRoom){
             }
         });
     });
+}
+
+function buildPaths(currentRoom){
+    _.each(currentRoom.find(FIND_MY_SPAWNS),function(spawn){
+        let spawnInformation = currentRoom.memory.spawnInfo;
+        // console.log(spawnInformation);
+        if(spawnInformation==undefined){
+            let spawnObject = {x:spawn.pos.x,y:spawn.pos.y};
+            let  spawnInfo={};
+            spawnInfo[spawn.id] = spawnObject;
+            currentRoom.memory.spawnInfo=spawnInfo;
+        }else if(!_.contains(spawnInformation,spawnInformation[spawn.id])){
+            let spawnObject = {x:spawn.pos.x,y:spawn.pos.y};
+            currentRoom.memory.spawnInfo[spawn.id] = spawnObject;
+        }
+        let targets = pathTargets(currentRoom);
+        // console.log(targets.length);
+        _.each(targets,function(target){
+            let tarPos = new RoomPosition(target.x,target.y,currentRoom.name);
+            let pathTiles = currentRoom.findPath(spawn.pos,tarPos,{ignoreCreeps:true});
+            pathTiles.forEach(function(dest){
+                // if(currentRoom.createConstructionSite(dest.x,dest.y,STRUCTURE_ROAD) != 0){
+                // }
+                currentRoom.createFlag(dest.x,dest.y,null,COLOR_RED);
+            });
+        })
+        
+    })
+}
+
+function pathTargets(currentRoom){
+    var targets = [];
+    currentRoom.find(FIND_SOURCES).forEach(function(source){
+        targets.push({x:source.pos.x,y:source.pos.y});
+    });
+    currentRoom.find(FIND_MY_SPAWNS).forEach(function(spawn){
+        let offset=1;
+        let xPos = spawn.pos.x - offset;
+        let yPos = spawn.pos.y - offset;
+        for(var x =xPos; x<=spawn.pos.x+offset ;x++){
+            for(var y=yPos;y<=spawn.pos.y+offset;y++){
+                //  if(x==spawn.pos.x+1 && y==spawn.pos.y+1){
+                //     currentRoom.createConstructionSite(x,y,STRUCTURE_CONTAINER);
+                //     continue;
+                // }
+                // currentRoom.createFlag(x,y);
+                let obj = {x:x,y:y};
+                // console.log(obj.x + " "+ obj.y);
+                targets.push(obj);
+            }
+        }
+        targets.push({x:spawn.pos.x,y:spawn.pos.y});
+    });
+    targets.push({x:currentRoom.controller.pos.x,y:currentRoom.controller.pos.y});
+    return targets;
 }
 module.exports = roomSetup;
