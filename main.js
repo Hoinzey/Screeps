db0abd216744d035981bd350da8f64006e7a0664
Darren Hoyne <hoinzey2007@yahoo.ie>
2018-03-07 21:38:49 +0000

    h

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ var creepManager = require('creepManager');
 module.exports.loop = function () {
     for(var currentRoomName in Game.rooms){
         var currentRoom = Game.rooms[currentRoomName];
-        buildPaths(JSON.stringify(currentRoom));
+        buildPaths(currentRoom);
         creepManager.run(currentRoom);
     }
 }
 
 function buildPaths(room){
     //room.createFlag(x,y,COLOR_RED)
-    console.log(room.find(FIND_MY_SPAWNS));
+    console.log(JSON.stringify( room.find(FIND_MY_SPAWNS)));
 
     //createConstructionSite
 }
